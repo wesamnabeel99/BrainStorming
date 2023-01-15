f24c141c2ef0c51c3150bd23fd559da0f8a111ce
Wesam Nabeel <wesamezio@gmail.com>
2023-01-16 00:24:54 +0300

    add todos'

--- app/src/main/java/com/wesam/brainstorming/model/repository/notes/NotesRepositoryImpl.kt
+++ app/src/main/java/com/wesam/brainstorming/model/repository/notes/NotesRepositoryImpl.kt
@@ -3,6 +3,8 @@ package com.wesam.brainstorming.model.repository.notes
 import com.wesam.brainstorming.model.local.entities.Note
 import com.wesam.brainstorming.model.local.database.NotesDatabase
 
+
+// TODO: convert to regular class and use HILT to inject the dependency
 object NotesRepositoryImpl : NotesRepository {
     private val notesDao = NotesDatabase.getInstanceWithoutContext().notesDao()
 
--- app/src/main/java/com/wesam/brainstorming/model/repository/words/WordsRepositoryImpl.kt
+++ app/src/main/java/com/wesam/brainstorming/model/repository/words/WordsRepositoryImpl.kt
@@ -3,6 +3,7 @@ package com.wesam.brainstorming.model.repository.words
 import com.wesam.brainstorming.model.remote.network.API
 import com.wesam.brainstorming.model.remote.network.ApiWrapper
 
+// TODO: convert to regular class and use HILT to inject the dependency
 object WordsRepositoryImpl : WordsRepository {
 
     override suspend fun getRecommendedWord(word: String) =
