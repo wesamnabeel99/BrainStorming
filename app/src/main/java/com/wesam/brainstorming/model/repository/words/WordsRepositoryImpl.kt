5ea5036df127fcd692a0051cc5d44aa6008e8720
Wesam Nabeel <wesamezio@gmail.com>
2023-01-21 03:40:04 +0300

    delete unnecessary api wrapper class

--- app/src/main/java/com/wesam/brainstorming/model/repository/words/WordsRepositoryImpl.kt
+++ app/src/main/java/com/wesam/brainstorming/model/repository/words/WordsRepositoryImpl.kt
@@ -1,13 +1,31 @@
 package com.wesam.brainstorming.model.repository.words
 
-import com.wesam.brainstorming.model.remote.network.ApiWrapper
+import com.wesam.brainstorming.model.remote.network.State
 import com.wesam.brainstorming.model.remote.network.WordService
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.Response
 import javax.inject.Inject
 
 class WordsRepositoryImpl @Inject constructor(
-    private val apiService: WordService
+    private val apiService: WordService,
 ) : WordsRepository {
+
+    private fun <T> wrapWithFlow(function: suspend () -> Response<T>): Flow<State<T?>> = flow {
+        emit(State.Loading)
+        try {
+            val result = function()
+            if (result.isSuccessful) {
+                emit(State.Success(result.body()))
+            } else {
+                emit(State.Error(result.message()))
+            }
+        } catch (e: Exception) {
+            emit(State.Error(e.message.toString()))
+        }
+    }
+
     override suspend fun getRecommendedWord(word: String) =
-        ApiWrapper.wrapWithFlow { apiService.getNextWord(word) }
+        wrapWithFlow { apiService.getNextWord(word) }
 }
 
